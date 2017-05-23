930855e723bb7cbf2fe4c8d9ae42fab9cd1fd2e9
dave031387@outlook.com <dave031387@outlook.com>
2017-05-22 21:31:08 -0400

    build 5/22/2017

---
+++ DataPod/DataPod.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FileIO;
+
+namespace DataPod
+{
+    public class DataPod
+    {
+        public string Name { get; private set; }
+        private List<string> _fieldNames;
+        private List<string[]> _records;
+        private EncodedTextFile _file;
+    }
+}
